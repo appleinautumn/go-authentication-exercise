ea39ad611a8fd6e11c1bb272791260e8cec3aae2
yu <mail@firdaus.org>
2023-06-27 04:19:09 +0700

    Add list method

--- user/repository/interface.go
+++ user/repository/interface.go
@@ -7,6 +7,7 @@ import (
 )
 
 type UserRepository interface {
+	List(ctx context.Context) ([]*entity.User, error)
 	FindOneByUsername(ctx context.Context, username string) (*entity.User, error)
 	Create(ctx context.Context, u *entity.User) (*entity.User, error)
 }
