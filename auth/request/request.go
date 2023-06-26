e496bd15f55b0853687bcdaaace74fda40f39e79
yu <mail@firdaus.org>
2023-06-27 03:50:43 +0700

    Add Login request

--- auth/request/request.go
+++ auth/request/request.go
@@ -1,5 +1,10 @@
 package request
 
+type LoginRequest struct {
+	Username string `json:"username" validate:"required,min=2"`
+	Password string `json:"password" validate:"required,min=5"`
+}
+
 type SignupRequest struct {
 	Username string `json:"username" validate:"required,min=2"`
 	Fullname string `json:"fullname" validate:"required"`
