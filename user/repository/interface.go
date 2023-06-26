46dc2db695f88aa5899d1b9081020c4ac80add09
yu <mail@firdaus.org>
2023-06-27 03:17:52 +0700

    Add FindOneByUsername and Create method

--- user/repository/interface.go
+++ user/repository/interface.go
@@ -7,7 +7,6 @@ import (
 )
 
 type UserRepository interface {
-	List(ctx context.Context) ([]*entity.User, error)
 	FindOneByUsername(ctx context.Context, username string) (*entity.User, error)
 	Create(ctx context.Context, u *entity.User) (*entity.User, error)
 }
