1b5ef51e85eed931c7acd99e388c918da33caff1
Firdaus Adi Putra <mail@firdaus.org>
2025-03-03 09:42:16 +0700

    Update project name

--- main.go
+++ main.go
@@ -26,7 +26,11 @@ func main() {
 	}
 
 	//  init database
-	connStr := "postgres://postgres:love@localhost/go_auth_db?sslmode=disable"
+	connStr := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_NAME"))
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
